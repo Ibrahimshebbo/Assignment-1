533618d43a354ce69d65af7f695f469c470cd011
Ibrahim <Ibrahimshebbo5@gmail.com>
2025-02-13 01:51:39 +0200

    second commit

---
+++ login system .py
@@ -0,0 +1,7 @@
+username = str(input("Enter username:"))
+password = str(input("Enter password:"))
+if username == "admin" and password == "1234":
+    print("Access granted")
+else:
+    print("Access denied")
+
